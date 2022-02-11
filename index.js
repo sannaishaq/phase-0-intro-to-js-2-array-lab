7713de3fa0f298b18ec836e6e8e772649c5334e9
Sanna Ishaq <sakinah.ishaq@gmail.com>
2022-02-11 11:55:55 -0500

    Completed Assignment:
    git push
    cd ..

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(cat){cats.push(cat);}
+function destructivelyPrependCat(cat){cats.unshift(cat);}
+function destructivelyRemoveLastCat(cat){cats.pop(cat);}
+function destructivelyRemoveFirstCat(cat){cats.shift(cat);}
+function appendCat(name){
+    const newCats = cats.slice();
+    newCats.push(name);
+    return newCats;
+}
+function  prependCat(name){
+    const newCat = cats.slice();
+    newCat.unshift(name);
+    return newCat;
+}
+function  removeFirstCat(){
+    const newCatss = cats.slice();
+    newCatss.shift();
+    return newCatss;
+}
+function removeLastCat(){
+    const newCatsss = cats.slice();
+    newCatsss.pop();
+    return newCatsss;
+}
