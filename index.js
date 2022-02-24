18e7ab2741a0d1d3a2fd339246c3138d060c6cc5
Sanna Ishaq <sakinah.ishaq@gmail.com>
2022-02-24 13:44:20 -0500

    commit tree 091e83d8e7879a74ade68cad8085ee4e5dd99f2c

--- index.js
+++ index.js
@@ -1,25 +1,73 @@
-const cats = ["Milo", "Otis", "Garfield"];
-function destructivelyAppendCat(cat){cats.push(cat);}
-function destructivelyPrependCat(cat){cats.unshift(cat);}
-function destructivelyRemoveLastCat(cat){cats.pop(cat);}
-function destructivelyRemoveFirstCat(cat){cats.shift(cat);}
-function appendCat(name){
-    const newCats = cats.slice();
-    newCats.push(name);
-    return newCats;
+function cats([]){
+   let newCats =  ["Milo", "Otis", "Garfield"]
 }
-function  prependCat(name){
-    const newCat = cats.slice();
-    newCat.unshift(name);
-    return newCat;
-}
-function  removeFirstCat(){
-    const newCatss = cats.slice();
-    newCatss.shift();
-    return newCatss;
-}
-function removeLastCat(){
-    const newCatsss = cats.slice();
-    newCatsss.pop();
-    return newCatsss;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const cats = ["Milo", "Otis", "Garfield"];
+// function destructivelyAppendCat(cat){cats.push(cat);}
+// function destructivelyPrependCat(cat){cats.unshift(cat);}
+// function destructivelyRemoveLastCat(cat){cats.pop(cat);}
+// function destructivelyRemoveFirstCat(cat){cats.shift(cat);}
+// function appendCat(name){
+//     const newCats = cats.slice();
+//     newCats.push(name);
+//     return newCats;
+// }
+// function  prependCat(name){
+//     const newCat = cats.slice();
+//     newCat.unshift(name);
+//     return newCat;
+// }
+// function  removeFirstCat(){
+//     const newCatss = cats.slice();
+//     newCatss.shift();
+//     return newCatss;
+// }
+// function removeLastCat(){
+//     const newCatsss = cats.slice();
+//     newCatsss.pop();
+//     return newCatsss;
+// }
